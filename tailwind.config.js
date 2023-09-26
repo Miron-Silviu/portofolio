77f84df5b8f72cb6bd65f5c87e0d6bba1b016961
Silviu Miron <silviu_miron_90@outlook.com>
2023-09-27 00:42:38 +0300

    Fixed the header section , the main section and footer section was added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
 
     screens: {
-      'xs': {'min': '10px', 'max': '399px'},    
-      'sm': {'min': '400px', 'max': '767px'},
+      'xs': {'min': '10px', 'max': '199px'},    
+      'sm': {'min': '200px', 'max': '500px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      'md': {'min': '768px', 'max': '1023px'},
+      'md': {'min': '501px', 'max': '1023px'},
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
       'lg': {'min': '1024px', 'max': '1279px'},
